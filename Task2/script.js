74aeb8c18f0b5c028b3b88ecf69bb70460ddff55
Dasari Vamshi <86954980+vamshiashok@users.noreply.github.com>
2025-02-19 13:06:48 +0530

    Add files via upload
    Task 2 Event Schedule Page

---
+++ Task2/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault(); 
+        const targetId = this.getAttribute('href'); 
+        const targetElement = document.querySelector(targetId); 
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
